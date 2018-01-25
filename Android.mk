2275a48282b03ceb0e3c372a7a81056a72e4578b
James Lee <james.lee@twobulls.com>
2018-01-25 22:00:26 +1100

    Simplify Android.mk file

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-libusb_files := \
+LOCAL_SRC_FILES := \
 	libusb/core.c \
 	libusb/descriptor.c \
 	libusb/hotplug.c \
@@ -13,25 +13,16 @@ libusb_files := \
 	libusb/os/linux_usbfs.c \
 	libusb/os/linux_netlink.c
 
-libusb_include_files := \
-	$(LOCAL_PATH)/libusb/ \
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH)/libusb \
 	$(LOCAL_PATH)/libusb/os \
-	$(LOCAL_PATH)/android/
+	$(LOCAL_PATH)/android
 
 LOCAL_MODULE := libusb
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -O3 -Wall -Wno-error=sign-compare -Wno-error=switch -Werror
 
-LOCAL_SRC_FILES := $(libusb_files)
-LOCAL_C_INCLUDES = $(libusb_include_files)
 LOCAL_SHARED_LIBRARIES := liblog
-LOCAL_EXPORT_C_INCLUDE_DIRS := include
-
-ifneq ($(TARGET_BUILD_APPS),)
-ifeq ($(TARGET_ARCH),arm)
-  LOCAL_SDK_VERSION := 9
-endif
-endif
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 include $(BUILD_SHARED_LIBRARY)
 
